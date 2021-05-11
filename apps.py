ddb0540e85d8ca2f6e02fbaaaa0a076a5509b69e
Chris J. Karr <chris@audacious-software.com>
2021-05-11 19:39:28 +0000

    Initial project commit

---
+++ apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class PassiveDataKitCodebookConfig(AppConfig):
+    name = 'passive_data_kit_codebook'
