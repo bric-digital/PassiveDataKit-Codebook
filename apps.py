84533014f6f3aaa6b7cdb1bcf095633c65c94d01
Chris J. Karr <chris@audacious-software.com>
2021-12-28 18:11:54 +0000

    Specifying default ID type

--- apps.py
+++ apps.py
@@ -6,3 +6,4 @@ from django.apps import AppConfig
 class PassiveDataKitCodebookConfig(AppConfig):
     name = 'passive_data_kit_codebook'
     verbose_name = 'Passive Data Kit: Codebook'
+    default_auto_field = 'django.db.models.AutoField'
